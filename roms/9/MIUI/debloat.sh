6a2436a1cfb91483aa6d40c38e725f06a520a4f7
AdaM <37734266+adamayyad@users.noreply.github.com>
2020-06-11 10:58:52 +0300

    Update debloat.sh

--- roms/9/MIUI/debloat.sh
+++ roms/9/MIUI/debloat.sh
@@ -7,13 +7,11 @@ rm -rf $1/app/cit
 rm -rf $1/app/MiuiCompass
 rm -rf $1/app/MiuiScreenRecorder
 rm -rf $1/app/MiuiVideoGlobal
-rm -rf $1/app/Notes
 rm -rf $1/app/PaymentService
 rm -rf $1/app/Calculator
 rm -rf $1/priv-app/Backup
 rm -rf $1/priv-app/Browser
 rm -rf $1/priv-app/Calendar
-rm -rf $1/priv-app/CleanMaster
 rm -rf $1/priv-app/MiRecycle
 rm -rf $1/priv-app/MiuiScanner
 rm -rf $1/priv-app/MiWebView
@@ -21,26 +19,19 @@ rm -rf $1/priv-app/Updater
 rm -rf $1/priv-app/Velvet
 rm -rf $1/priv-app/Weather
 rm -rf $1/priv-app/WeatherProvider
-
-# add more apps to debloat
-
 rm -rf $1/app/VoiceAssist
 rm -rf $1/app/MiRadio
 rm -rf $1/app/Mipay
 rm -rf $1/app/NexyPay
 rm -rf $1/app/com.miui.qr
-rm -rf $1/app/FM
 rm -rf $1/app/MiuiBugReport
-
 rm -rf $1/priv-app/MiuiVideo
 rm -rf $1/priv-app/PersonalAssistant
 rm -rf $1/priv-app/MiService
-#rm -rf $1/priv-app/Music
-
+rm -rf $1/priv-app/Music
 rm -rf $1/data-app/MiShop
 rm -rf $1/data-app/XiaoAiSpeechEngine
 rm -rf $1/data-app/Email
-
 rm -rf $1/data-app/BaiduIME
 rm -rf $1/data-app/MiFinance
 rm -rf $1/data-app/MiuiScanner
@@ -50,7 +41,6 @@ rm -rf $1/data-app/Huanji
 rm -rf $1/data-app/VirtualSim
 rm -rf $1/data-app/VipAccount
 rm -rf $1/data-app/GameCenter
-
 rm -rf $1/data-app/com.sina.weibo
 rm -rf $1/data-app/com.taobao.taobao
 rm -rf $1/data-app/Youpin
@@ -70,18 +60,14 @@ rm -rf $1/data-app/XMPass
 rm -rf $1/data-app/SmartTravel
 rm -rf $1/data-app/MiDrive
 rm -rf $1/data-app/com.eg.android.AlipayGphone
-
 rm -rf $1/app/AnalyticsCore
 rm -rf $1/app/BasicDreams
 rm -rf $1/app/BugReport
 rm -rf $1/app/Drive
 rm -rf $1/app/Duo
-rm -rf $1/app/FM
-rm -rf $1/app/FileExplorer_old
 rm -rf $1/app/Gmail2
 rm -rf $1/app/HybridAccessory
 rm -rf $1/app/HybridPlatform
-rm -rf $1/app/IdMipay
 rm -rf $1/app/InMipay
 rm -rf $1/app/MSA-Global
 rm -rf $1/app/Maps
@@ -95,6 +81,8 @@ rm -rf $1/app/TranslationService
 rm -rf $1/app/Videos
 rm -rf $1/app/YouDaoEngine
 rm -rf $1/app/YouTube
+rm -rf $1/app/XiaomiServiceFramework
+rm -rf $1/priv-app/Provision
 rm -rf $1/app/facebook-appmanager
 rm -rf $1/app/talkback
 rm -rf $1/data-app/*
@@ -103,23 +91,41 @@ rm -rf $1/priv-app/GameCenterGlobal
 rm -rf $1/priv-app/GlobalUserGuide
 rm -rf $1/priv-app/MiDrop
 rm -rf $1/priv-app/MiuiBrowserGlobal
-rm -rf $1/priv-app/MiuiScanner
-rm -rf $1/priv-app/Music
 rm -rf $1/priv-app/PersonalAssistantGlobal
 rm -rf $1/priv-app/SoundRecorder
 rm -rf $1/priv-app/Turbo
-rm -rf $1/priv-app/Weather
 rm -rf $1/priv-app/YellowPage
 rm -rf $1/priv-app/facebook-installer
 rm -rf $1/priv-app/facebook-services
-#rm -rf $1/priv-app/MiService
 rm -rf $1/app/Lens
-
-
-#rm -rf $1/fonts/NotoSansEgyptianHieroglyphs-Regular.ttf
-#rm -rf $1/fonts/NotoSansTibetan-Bold.ttf
-#rm -rf $1/fonts/NotoSansTibetan-Regular.ttf
-#rm -rf $1/fonts/NotoSerifCJK-Regular.ttc
-#rm -rf $1/fonts/NotoSansCJK-Regular.ttc
-#rm -rf $1/fonts/NotoSansCuneiform-Regular.ttf
-#rm -rf $1/media/theme/miui_mod_icons
+rm -rf $1/product/priv-app/Velvet
+rm -rf $1/product/priv-app/AndroidAutoStub
+rm -rf $1/product/priv-app/HotwordEnrollmentOKGoogleHEXAGON
+rm -rf $1/product/priv-app/Turbo
+rm -rf $1/product/priv-app/HotwordEnrollmentXGoogleHEXAGON
+rm -rf $1/product/app/Chrome
+rm -rf $1/product/app/Gmail2
+rm -rf $1/product/app/GoogleTTS
+rm -rf $1/product/app/Maps
+rm -rf $1/product/app/YouTube
+rm -rf $1/product/app/talkback
+rm -rf $1/priv-app/MiShare
+rm -rf $1/priv-app/Tag
+rm -rf $1/priv-app/MiuiMusicGlobal
+rm -rf $1/priv-app/MiCalendarGlobal
+rm -rf $1/priv-app/MiBrowserGlobal
+rm -rf $1/priv-app/CleanMaster
+rm -rf $1/app/CatchLog
+rm -rf $1/app/Joyose
+rm -rf $1/app/GuardProvider
+rm -rf $1/app/MSA-Global
+rm -rf $1/app/Zman
+rm -rf $1/app/mi_connect_service
+rm -rf $1/app/CtsShimPrebuilt
+rm -rf $1/app/FM
+rm -rf $1/app/FileExplorer_old
+rm -rf $1/app/Notes
+rm -rf $1/app/Netflix_activation
+rm -rf $1/app/IdMipay
+rm -rf $1/app/AnalyticsCore
+rm -rf $1/data-app/MiCreditInStub
