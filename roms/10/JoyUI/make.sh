1b18d23e4305e351b90d301bc3df0f9c6274d211
JamieHoSzeYui <henloboii@protonmail.ch>
2020-11-07 03:55:57 +0000

    JoyUI Q : Fix systemUI crash due to FOD

--- roms/10/JoyUI/make.sh
+++ roms/10/JoyUI/make.sh
@@ -10,17 +10,18 @@ cp -fpr $thispath/etc/* $1/etc/
 cp -fpr $thispath/bin/* $1/bin/
 cp -fpr $thispath/product/* $1/product/
 
-# Drop SetupWizard
-rm -rf $1/priv-app/ZsSetupWizard
 # Force provision on build.prop and forceprovision.rc
 echo "DEVICE_PROVISIONED=1" >> $1/build.prop
 
 # Set locale to en-US
 sed -i 's/zh-CN/en-US/g' $1/build.prop
 
-# Prevent .dataservices crashes
-rm -rf $1/priv-app/dpmserviceapp/
-
 # Custom Manifest   
 python $thispath/../../../scripts/custom_manifest.py $thispath/../../../tmp/manifest.xml $thispath/manifest.xml $1/etc/vintf/manifest.xml
 cp -fpr $thispath/../../../tmp/manifest.xml $1/etc/vintf/manifest.xml
+
+# Blackshark Q FOD Workaround
+echo "ro.hardware.fp.fod=true" >> $1/build.prop
+echo "persist.vendor.sys.fp.fod.location.X_Y=445,1260" >> $1/build.prop
+echo "persist.vendor.sys.fp.fod.size.width_height=190,190" >> $1/build.prop
+echo "DEVICE_PROVISIONED=1" >> $1/build.prop
