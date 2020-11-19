34d3c5c9982d9b3a74ed464f088958aec3243d3b
AdaMda | Nipponiee <37734266+nnippon@users.noreply.github.com>
2020-11-19 18:35:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     else
        sudo apt install unace unrar zip unzip p7zip-full simg2img p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2 unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller rename liblzma-dev python-pip brotli -y
     fi
-    pip install backports.lzma protobuf pycrypto bsdiff4
+    pip install backports.lzma protobuf pycrypto bsdiff4 docopt
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install protobuf xz brotli lz4 aria2
-    pip install backports.lzma protobuf pycrypto bsdiff4
+    pip install backports.lzma protobuf pycrypto bsdiff4 docopt
 fi
