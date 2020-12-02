bf1e087a3934d5d5f4bd521fc9bb4633108b66a0
nnippon <adameyyad999@gmail.com>
2020-12-02 17:42:06 +0000

    Proper fix for Dynamic script
    Signed-off-by: nnippon <adameyyad999@gmail.com>

--- dyn.sh
+++ dyn.sh
@@ -1,58 +1,36 @@
 #!/bin/bash
 
 #Variables
-
-PARTITIONS=("system" "product" "opproduct" "reserve" "vendor" "system_ext")
 LOCALDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-payload_extractor="tools/update_payload_extractor/extract.py"
+payload_extractor="$LOCALDIR/payload/payload_dumper.py"
 outdir="$LOCALDIR/cache"
 tmpdir="$LOCALDIR/cache/tmp"
-#############################################################
 
-usage() {
-    echo "Usage: $0 <Firmware Type> [Path to Firmware]"
-    echo -e "\tFirmware Type! = OxygenOS or Pixel"
-    echo -e "\tPath to Firmware!"
-}
-
-if [ "$1" == "" ]; then
-    echo "Enter all needed parameters"
-    usage
-    exit 1
-fi
-
-echo "Create Temp and out dir"
+#let's start
+echo "Create temp and cache dir"
 	mkdir -p "$outdir"
 	mkdir -p "$tmpdir"
-	unzip $2 -d $tmpdir &> /dev/null
 
 echo "Extracting Required Partitions . . . . "
 if [ $1 = "Generic" ]; then
 	bash $LOCALDIR/zip2img.sh $2 $outdir
- 	bash $LOCALDIR/zip2img.sh $2 $outdir -p
+ 	bash $LOCALDIR/zip2img.sh $2 $outdir --product
 	mv $outdir/system.img $outdir/system-old.img
 elif [ $1 = "MIUI" ]; then
 	bash $LOCALDIR/zip2img.sh $2 $outdir
- 	bash $LOCALDIR/zip2img.sh $2 $outdir -p
+ 	bash $LOCALDIR/zip2img.sh $2 $outdir --product
 	mv $outdir/system.img $outdir/system-old.img
 elif [ $1 = "OxygenOS" ]; then
-		for partition in ${PARTITIONS[@]}; do
- 	   	    python $payload_extractor --partitions $partition --output_dir $tmpdir $tmpdir/payload.bin 
-		done
-	mv $tmpdir/system $outdir/system-old.img
-	mv $tmpdir/product $outdir/product.img
-	mv $tmpdir/opproduct $outdir/opproduct.img
-	mv $tmpdir/vendor $outdir/vendor.img
-	mv $tmpdir/reserve $outdir/reserve.img
- 	if [ $(echo -n $1 | tail -c 1) = "R" ]; then
-	   mv $tmpdir/system_ext.img $outdir/system_ext.img
-	fi
-elif [ $(echo -n $1 | head -c 5) = "Pixel" ]; then
+        unzip $2 -d $tmpdir &> /dev/null
+        python $payload_extractor --out $outdir $tmpdir/payload.bin &> /dev/null
+        mv $outdir/system.img $outdir/system-old.img
+elif [ $1 = "Pixel" ]; then
+        unzip $2 -d $tmpdir &> /dev/null
 	unzip $tmpdir/*/*.zip -d $tmpdir &> /dev/null
 	simg2img $tmpdir/system.img $outdir/system-old.img
 	simg2img $tmpdir/product.img $outdir/product.img
 	simg2img $tmpdir/system_other.img $outdir/system_other.img
- 	if [ $(echo -n $1 | tail -c 1) = "R" ]; then
+ 	if [ -f "$tmpdir/system_ext.img" ]; then
 	    simg2img $tmpdir/system_ext.img $outdir/system_ext.img
 	fi
 fi
@@ -61,31 +39,31 @@ echo "Creating Dummy System Image . . . . "
 dd if=/dev/zero of=$outdir/system.img bs=4k count=1048576
 mkfs.ext4 $outdir/system.img
 tune2fs -c0 -i0 $outdir/system.img
-echo "Mounting System Images . . . . "
+echo "Merging system . . . . "
 	mkdir system
 	mkdir system-old
 	mount -o loop $outdir/system.img system/
 	mount -o ro $outdir/system-old.img system-old/
-	echo "  "
-echo "Copying Files . . . . "
 	cp -v -r -p system-old/* system/ &> /dev/null
 	sync
 	umount system-old
 	rm $outdir/system-old.img
-	rm -rf system/product
-	ln -s system/product system/product
-    	rm -rf system/system/product
-    	mkdir system/system/product/
-echo "Merging product.img "
+        rmdir system-old/
+echo "Merging product . . . . "
+        rm -rf system/product
+        ln -s system/product system/product
+        rm -rf system/system/product
+        mkdir system/system/product
 	sudo mkdir $outdir/product
-	mount -o ro $outdir/product.img $outdir/product/
+	mount -o ro $outdir/product.img $outdir/product
 	cp -v -r -p $outdir/product/* system/system/product/ &> /dev/null
 	sync
 	umount $outdir/product
 	rmdir $outdir/product/
 	rm $outdir/product.img
 if [ $1 = "OxygenOS" ]; then
-	echo "Merging opproduct.img "
+     if [ -f "$outdir/opproduct.img" ]; then
+	echo "Merging opproduct . . . . "
 	sudo mkdir $outdir/opproduct
 	mount -o ro $outdir/opproduct.img $outdir/opproduct/
 	cp -v -r -p $outdir/opproduct/* system/oneplus/ &> /dev/null
@@ -93,7 +71,9 @@ if [ $1 = "OxygenOS" ]; then
 	umount $outdir/opproduct
 	rmdir $outdir/opproduct/
 	rm $outdir/opproduct.img
-	echo "Merging reserve.img "
+     fi
+     if [ -f "$outdir/reserve.img" ]; then
+	echo "Merging reserve . . . . "
 	sudo mkdir $outdir/reserve
 	mount -o ro $outdir/reserve.img $outdir/reserve/
 	cp -v -r -p $outdir/reserve/* system/system/reserve/ &> /dev/null
@@ -101,19 +81,20 @@ if [ $1 = "OxygenOS" ]; then
 	umount $outdir/reserve
 	rmdir $outdir/reserve/
 	rm $outdir/reserve.img
-	echo "Merging overlays "
+     fi
+	echo "Merging overlays . . . . "
 	sudo mkdir $outdir/vendor
 	mount -o ro $outdir/vendor.img $outdir/vendor/
 	cp -r $outdir/vendor/overlay $outdir
 	rm -rf $outdir/overlay/*.apk
 	cp -v -r -p $outdir/overlay/* system/system/product/overlay/ &> /dev/null
 	sync
-	rmdir $outdiroverlay
+	rmdir $outdir/overlay/
 	umount $outdir/vendor
 	rmdir $outdir/vendor/
 	rm $outdir/vendor.img
-	if [  $(echo -n $1 | tail -c 1) = "R" ]; then
-        echo "Merging system_ext.img "
+     if [ -f "$outdir/system_ext.img" ]; then
+        echo "Merging system_ext . . . . "
 	    sudo mkdir $outdir/system_ext
 	    mount -o ro $outdir/system_ext.img $outdir/system_ext/
 	    rm -rf system/system_ext
@@ -125,18 +106,19 @@ if [ $1 = "OxygenOS" ]; then
 	    umount $outdir/system_ext
 	    rmdir $outdir/system_ext/
 	    rm $outdir/system_ext.img
-elif [ $(echo -n $1 | head -c 5) = "Pixel" ]; then
-echo "Merging system_other.img "
-	sudo mkdir $outdir/system_other
+     fi
+elif [ $1 = "Pixel" ]; then
+echo "Merging system_other . . . . "
+	mkdir $outdir/system_other
 	mount -o ro $outdir/system_other.img $outdir/system_other/
 	cp -v -r -p $outdir/system_other/* system/system/ &> /dev/null
 	sync
 	umount $outdir/system_other
 	rmdir $outdir/system_other/
 	rm $outdir/system_other.img
-    if [  $(echo -n $1 | tail -c 1) = "R" ]; then
-        echo "Merging system_ext.img "
-	    sudo mkdir $outdir/system_ext
+    if [ -f "$outdir/system_ext.img" ]; then
+        echo "Merging system_ext . . . . "
+	    mkdir $outdir/system_ext
 	    mount -o ro $outdir/system_ext.img $outdir/system_ext/
 	    rm -rf system/system_ext
 	    rm -rf system/system/system_ext
@@ -150,8 +132,10 @@ echo "Merging system_other.img "
     fi
 fi
 echo "Finalising "
-        sudo cp -r system/ $LOCALDIR/working
+        mkdir working/
+        cp -r system working/ &> /dev/null
 	umount system
-	rm -rf system/
-	rm -rf system-old/
+        rm -rf cache
+	rm -rf system
+        rm -rf $outdir/system.img
 echo "Dynamic Script Done"
