9d69437b0f759ff723dd46201388264139193d6a
nnippon <adameyyad999@gmail.com>
2021-01-28 19:10:24 +0000

    Fix setup.sh
    Signed-off-by: nnippon <adameyyad999@gmail.com>

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
        sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2
        #aur=rar
     else
-       sudo apt install unace unrar zip unzip p7zip-full simg2img p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2 unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller rename liblzma-dev python-pip brotli -y
+       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev brotli liblz4-tool axel gawk aria2 detox cpio rename build-essential simg2img aria2 python-pip -y
     fi
-    pip install setuptools wheel backports.lzma protobuf pycrypto bsdiff4 docopt
+    pip install setuptools wheel backports.lzma docopt zstandard bsdiff4 backports.lzma protobuf pycrypto
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install protobuf xz brotli lz4 aria2
     pip install setuptools wheel backports.lzma protobuf pycrypto bsdiff4 docopt
