f94aa4bc1a11f4726710aa86c665f85f1d58c151
Nippon <aayyad9@students.iugaza.edu.ps>
2021-02-08 06:05:21 +0000

    Only Ubuntu cause YES
    Signed-off-by: Nippon <aayyad9@students.iugaza.edu.ps>

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
+# Install necessarily packages for run tools on Ubuntu-18.04 re
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
-    if [[ "$distro" == "arch" ]]; then
-       echo "Arch Linux Detected"
-       sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2
-       #aur=rar
-    else
-       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev brotli liblz4-tool axel gawk aria2 detox cpio rename build-essential simg2img aria2 python-pip -y
-    fi
-    pip install setuptools wheel backports.lzma docopt zstandard bsdiff4 backports.lzma protobuf pycrypto
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install protobuf xz brotli lz4 aria2
-    pip install setuptools wheel backports.lzma protobuf pycrypto bsdiff4 docopt
-fi
+sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev brotli liblz4-tool axel gawk aria2 detox cpio rename build-essential simg2img aria2 python-pip -y
+pip install wheel setuptools
+pip install backports.lzma docopt zstandard bsdiff4 protobuf pycrypto
