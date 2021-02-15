a5bb30ce41278a45ad721bc0a952bd69fbf78f1e
nnippon <adameyyad999@gmail.com>
2021-02-15 08:59:36 +0000

    Reworked dyn.sh
    Signed-off-by: nnippon <adameyyad999@gmail.com>

--- dyn.sh
+++ dyn.sh
@@ -7,12 +7,15 @@ payload_extractor="$LOCALDIR/payload/payload_dumper.py"
 outdir="$LOCALDIR/cache"
 tmpdir="$LOCALDIR/cache/tmp"
 
-echo "Create temp and cache dir"
+echo "Creating folders to start process . . . . "
 	mkdir -p "$outdir"
 	mkdir -p "$tmpdir"
+        mkdir working && mkdir working/system
+        mkdir system-old
 
-echo "Extracting Required Partitions . . . . "
+echo "Extracting ROM zip process started, will take some time . . . . "
 if [ $1 = "Generic" ]; then
+        echo "Generic OTA Detected, Extracting . . . . "
         unzip $2 -d $tmpdir &> /dev/null
         if [ -f "$tmpdir/payload.bin" ]
         then
@@ -22,6 +25,7 @@ if [ $1 = "Generic" ]; then
         fi
         mv $outdir/system.img $outdir/system-old.img
 elif [ $1 = "MIUI" ]; then
+        echo "MIUI OTA Detected, Extracting . . . . "
 	unzip $2 -d $tmpdir &> /dev/null
         if [ -f "$tmpdir/payload.bin" ]
         then
@@ -31,10 +35,12 @@ elif [ $1 = "MIUI" ]; then
         fi
 	mv $outdir/system.img $outdir/system-old.img
 elif [ $1 = "OxygenOS" ]; then
+        echo "OnePlus OTA Detected, Extracting . .  . ."
         unzip $2 -d $tmpdir &> /dev/null
         python $payload_extractor --out $outdir $tmpdir/payload.bin &> /dev/null
         mv $outdir/system.img $outdir/system-old.img
 elif [ $1 = "Pixel" ]; then
+        echo "Pixel OTA Detected, Extracting . . . . "
         unzip $2 -d $tmpdir &> /dev/null
 	unzip $tmpdir/*/*.zip -d $tmpdir &> /dev/null
 	simg2img $tmpdir/system.img $outdir/system-old.img
@@ -46,122 +52,95 @@ elif [ $1 = "Pixel" ]; then
 	simg2img $tmpdir/system_ext.img $outdir/system_ext.img
 	fi
 fi
+
+echo "Clear temp folder . . . . "
 rm -rf $tmpdir
-echo "Creating Dummy System Image . . . . "
-dd if=/dev/zero of=$outdir/system.img bs=4k count=1048576
-mkfs.ext4 $outdir/system.img
-tune2fs -c0 -i0 $outdir/system.img
+
+echo "Done extract, continue . . . . "
+echo "Creating 4GB empty image to fit merging . . . . "
+
+dd if=/dev/zero of=system.img bs=4k count=1048576
+mkfs.ext4 system.img
+tune2fs -c0 -i0 system.img
+
+echo "Done, merge process started, will take some time . . . . "
 echo "Merging system . . . . "
-	mkdir system
-	mkdir system-old
-	mount -o loop $outdir/system.img system/
+	mount -o loop system.img working/system/
 	mount -o ro $outdir/system-old.img system-old/
-	cp -v -r -p system-old/* system/ &> /dev/null
+	cp -v -r -p system-old/* working/system/ &> /dev/null
         sync
-	umount system-old
-	rm -rf $outdir/system-old.img
-        rm -rf system-old/
+	umount -l system-old
 if [ -f "$outdir/product.img" ]; then
 echo "Merging product . . . . "
-        rm -rf system/product
-        ln -s system/product system/product
-        rm -rf system/system/product
-        mkdir system/system/product
+        rm -rf working/system/product
+        ln -s working/system/product working/system/product
+        rm -rf working/system/system/product
+        mkdir working/system/system/product
 	mkdir $outdir/product
 	mount -o ro $outdir/product.img $outdir/product
-	cp -v -r -p $outdir/product/* system/system/product/ &> /dev/null
+	cp -v -r -p $outdir/product/* working/system/system/product/ &> /dev/null
 	sync
-        umount $outdir/product
-	rm -rf $outdir/product/
-	rm -rf $outdir/product.img
-fi
-if [ -f "$outdir/my_product.img" ]; then
-echo "Merging my_product . . . . "
-        rm -rf system/my_product
-        mkdir system/my_product
-        mkdir $outdir/my_product
-        mount -o ro $outdir/my_product.img $outdir/my_product
-        cp -v -r -p $outdir/my_product/* system/my_product/ &> /dev/null
-        sync
-        umount $outdir/my_product
-        rm -rf $outdir/my_product/
-        rm -rf $outdir/my_product.img
+        umount -l $outdir/product
 fi
 if [ -f "$outdir/system_ext.img" ]; then
 echo "Merging system_ext . . . . "
         mkdir $outdir/system_ext
         mount -o ro $outdir/system_ext.img $outdir/system_ext/
-        rm -rf system/system_ext
-        rm -rf system/system/system_ext
-        mkdir system/system/system_ext
-        ln -s system/system_ext system/system_ext
-        cp -v -r -p $outdir/system_ext/* system/system/system_ext/ &> /dev/n$
+        rm -rf working/system/system_ext
+        rm -rf working/system/system/system_ext
+        mkdir working/system/system/system_ext
+        ln -s working/system/system_ext working/system/system_ext
+        cp -v -r -p $outdir/system_ext/* working/system/system/system_ext/ &> /dev/n$
         sync
-        umount $outdir/system_ext
-        rm -rf $outdir/system_ext/
-        rm -rf $outdir/system_ext.img
+        umount -l $outdir/system_ext
 fi
 if [ $1 = "OxygenOS" ]; then
      if [ -f "$outdir/opproduct.img" ]; then
 	echo "Merging opproduct . . . . "
 	mkdir $outdir/opproduct
 	mount -o ro $outdir/opproduct.img $outdir/opproduct/
-	cp -v -r -p $outdir/opproduct/* system/oneplus/ &> /dev/null
+	cp -v -r -p $outdir/opproduct/* working/system/oneplus/ &> /dev/null
 	sync
-        umount $outdir/opproduct
-	rm -rf $outdir/opproduct/
-	rm -rf $outdir/opproduct.img
+        umount -l $outdir/opproduct
      fi
      if [ -f "$outdir/reserve.img" ]; then
 	echo "Merging reserve . . . . "
-        rm -rf system/system/reserve
-        mkdir system/system/reserve
+        rm -rf working/system/system/reserve
+        mkdir working/system/system/reserve
 	mkdir $outdir/reserve
 	mount -o ro $outdir/reserve.img $outdir/reserve/
-	cp -v -r -p $outdir/reserve/* system/system/reserve/ &> /dev/null
+	cp -v -r -p $outdir/reserve/* working/system/system/reserve/ &> /dev/null
 	sync
-        umount $outdir/reserve
-	rm -rf $outdir/reserve/
-	rm -rf $outdir/reserve.img
+        umount -l $outdir/reserve
      fi
      if [ -f "$outdir/india.img" ]; then
 	echo "Merging india . . . . "
-        rm -rf system/system/india
-        mkdir system/system/india
+        rm -rf working/system/system/india
+        mkdir working/system/system/india
 	mkdir $outdir/india
 	mount -o ro $outdir/india.img $outdir/india/
-	cp -v -r -p $outdir/india/* system/system/india/ &> /dev/null
+	cp -v -r -p $outdir/india/* working/system/system/india/ &> /dev/null
 	sync
-        umount $outdir/india
-	rm -rf $outdir/india/
-	rm -rf $outdir/india.img
+        umount -l $outdir/india
      fi
 echo "Merging overlays . . . . "
 	mkdir $outdir/vendor
 	mount -o ro $outdir/vendor.img $outdir/vendor/
 	cp -r $outdir/vendor/overlay $outdir
 	rm -rf $outdir/overlay/*.apk
-	cp -v -r -p $outdir/overlay/* system/system/product/overlay/ &> /dev/null
+	cp -v -r -p $outdir/overlay/* working/system/system/product/overlay/ &> /dev/null
 	sync
-        rm -rf $outdir/overlay
-	umount $outdir/vendor
-	rm -rf $outdir/vendor/
-	rm -rf $outdir/vendor.img
+	umount -l $outdir/vendor
 elif [ $1 = "Pixel" ]; then
 echo "Merging system_other . . . . "
 	mkdir $outdir/system_other
 	mount -o ro $outdir/system_other.img $outdir/system_other/
-	cp -v -r -p $outdir/system_other/* system/system/ &> /dev/null
+	cp -v -r -p $outdir/system_other/* working/system/system/ &> /dev/null
 	sync
-        umount $outdir/system_other
-	rm -rf $outdir/system_other/
-	rm -rf $outdir/system_other.img
+        umount -l $outdir/system_other
 fi
-echo "Finalising . . . . "
-        mkdir working
-        cp -r system working/ &> /dev/null
-        umount system
-        rm -rf $outdir/system.img
-        rm -rf cache
-	rm -rf system
-echo "Done"
+
+echo "Merge partitions done, clear cache . . . . "
+rm -rf $outdir
+
+echo "Done, now ready to build as GSI!"
