7791b5ab73e1c3bd14bcc74c333b1e5e00e69662
Nippon <37734266+nnippon@users.noreply.github.com>
2021-03-12 19:51:29 +0200

    Update debloat.sh

--- roms/S/Pixel/debloat.sh
+++ roms/S/Pixel/debloat.sh
@@ -3,35 +3,29 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-# small debloat
+# Delete Google apps for more space
 rm -rf $1/product/app/YouTube
 rm -rf $1/product/app/YouTubeMusicPrebuilt
 rm -rf $1/product/app/PrebuiltGmail
-rm -rf $1/product/app/PrebuiltBugle
 rm -rf $1/product/app/Maps
 rm -rf $1/product/app/Drive
 rm -rf $1/product/app/DiagnosticsToolPrebuilt
-rm -rf $1/product/app/Chrome
 rm -rf $1/product/app/CalendarGooglePrebuilt
 rm -rf $1/product/app/NgaResources
 rm -rf $1/product/priv-app/BetaFeedback
-rm -rf $1/product/priv-app/DevicePersonalizationPrebuiltPixel2020
 rm -rf $1/product/priv-app/HelpRtcPrebuilt
 rm -rf $1/product/priv-app/MyVerizonServices
 rm -rf $1/product/priv-app/OTAConfigPrebuilt
-rm -rf $1/product/priv-app/PixelLiveWallpaperPrebuilt
 rm -rf $1/product/priv-app/RecorderPrebuilt
 rm -rf $1/product/priv-app/SafetyHubPrebuilt
 rm -rf $1/product/priv-app/ScribePrebuilt
 rm -rf $1/app/datastatusnotification
 rm -rf $1/app/QAS_DVC_MSP_VZW
 rm -rf $1/app/VZWAPNLib
-rm -rf $1/app/ims
 rm -rf $1/app/vzw_msdc_api
 rm -rf $1/priv-app/CNEService
 rm -rf $1/priv-app/DMService
 rm -rf $1/priv-app/VzwOmaTrigger
-rm -rf $1/priv-app/qcrilmsgtunnel
 rm -rf $1/product/priv-app/ConnMO
 rm -rf $1/product/priv-app/DCMO
 rm -rf $1/product/priv-app/MyVerizonServices
@@ -46,11 +40,9 @@ rm -rf $1/product/app/WallpapersBReel*
 rm -rf $1/product/priv-app/EuiccSupportPixel
 rm -rf $1/product/priv-app/EuiccGoogle
 rm -rf $1/product/priv-app/WfcActivation
-
-# Fix Files-DocumentsUI
-rm -rf $1/product/overlay/PixelDocumentsUIOverlay
-
-# Some Unused Google Apps
 rm -rf $1/product/app/Music2
 rm -rf $1/product/app/Photos
 rm -rf $1/product/app/Videos
+
+# Fix Files-DocumentsUI
+rm -rf $1/product/overlay/PixelDocumentsUIOverlay
