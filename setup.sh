1be5536ddbf81ebb9869d4f518d33cc46146dd88
Nippon <37734266+nnippon@users.noreply.github.com>
2021-04-23 14:10:38 +0300

    setup.sh: Fix missing google module

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@
 sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev brotli liblz4-tool axel gawk aria2 detox cpio rename build-essential simg2img aria2 python3-pip default-jre -y
 pip3 install wheel setuptools
 pip3 install backports.lzma docopt zstandard bsdiff4 protobuf pycrypto
+sudo pip3 install --upgrade protobuf
